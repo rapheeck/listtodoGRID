ae66ac2ecd4b393a2f2a0d1b2fe05ded1b7c745b
Rafał Barnaś <125920366+rapheeck@users.noreply.github.com>
2023-03-16 21:17:15 +0100

    commit inital

---
+++ README.md
@@ -0,0 +1,20 @@
+# List to do
+
+## Demo
+
+[https://rapheeck.github.io/listtodo/]
+
+## Description
+
+An application for saving scheduled tasks to be performed.
+Gives you the ability to cross out or delete completed tasks.
+
+## Instruction
+
+Type tasks to do where the cursor is blinking.
+Press "enter" on the keyboard or click on the "Add task" button
+The green button deletes the task, the red button deletes it.
+
+## Screenshot
+
+![screenshot](https://raw.githubusercontent.com/rapheeck/listtodo/main/images/Zrzut%20ekranu%20(11).png)
---
+++ js/script.js
@@ -0,0 +1,78 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindRemoveEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+    };
+    const bindToggleDoneEvents = () => {
+        const toggleDoneButtons = document.querySelectorAll(".js-toggleDone");
+
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(taskIndex);
+            });
+        });
+    };F
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="tasks__item js-tasks">
+                <button class="js-toggleDone tasks__button tasks__button--toggleDone">${task.done ? "✔️" : ""}</button>
+                    <span class="tasks_content${task.done ? " tasks__content--done" : ""}">${task.content} </span>
+                <button class="js-remove tasks_button tasks__button--remove">❌</button>
+            </li>
+        `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindRemoveEvents();
+        bindToggleDoneEvents();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskContent.value = "";
+        }
+        newTaskElement.focus();
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+
+    };
+
+    init();
+}
